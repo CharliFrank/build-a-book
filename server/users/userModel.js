8755ac78e1585268a3fa63c70a12161c4c44b147
charlifrank <jayfrank0528@gmail.com>
2017-04-18 11:21:29 -0500

    added schema and model

---
+++ server/users/userModel.js
@@ -0,0 +1,18 @@
+const mongoose = require('mongoose');
+
+const UserSchema = new mongoose.Schema({
+  username: {
+    type: String,
+    required: true,
+    unique: true,
+  },
+
+  password: {
+    type: String,
+    required: true,
+  },
+});
+
+const UserModel = mongoose.model('users', UserSchema);
+module.exports = UserSchema;
+module.exports = UserModel;
