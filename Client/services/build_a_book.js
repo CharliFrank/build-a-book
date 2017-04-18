de552d22e8e7275b8aaa4073da874e9396ef1aa7
charlifrank <jayfrank0528@gmail.com>
2017-04-18 13:20:43 -0500

    Update client files build_a_book has a working input field

--- Client/services/build_a_book.js
+++ Client/services/build_a_book.js
@@ -3,4 +3,7 @@ angular.module('app.build_a_book', [])
 .controller('BuildABookController', function ($scope) {
   console.log('inside guild book app ctrl');
   $scope.message = 'Your controller is working! buildABookController';
+  $scope.savePage = function (text) {
+    console.log(text);
+  }
 });
