1d3cc61689589e5231eb1f34379c30f390ef3ff7
Operation Spark <opspark@operations-imac.attlocal.net>
2017-06-26 12:47:46 -0500

    Add aoptions for mongodb connection

--- server/server.js
+++ server/server.js
@@ -5,15 +5,26 @@ require('dotenv').config();
 const express = require('express');
 const mongoose = require('mongoose');
 
+const options = {
+  auth: {
+    authSource: 'admin',
+  },
+};
+
 const app = express();
 
-mongoose.connect(process.env.MONGODB);
+mongoose.connect(process.env.MONGODB, options, (err) => {
+  if (err) {
+    console.error(err);
+  } else {
+    app.listen(process.env.PORT || 3000, () => {
+      console.warn('Server is listening on port: ', process.env.PORT || 3000);
+    });
+  }
+});
 
 require('./config/middleware.js')(app, express);
 require('./config/routes.js')(app, express);
 
-app.listen(process.env.PORT || 3000, () => {
-  console.warn('Server is listening on port: ', process.env.PORT || 3000);
-});
 
 module.exports = app;
